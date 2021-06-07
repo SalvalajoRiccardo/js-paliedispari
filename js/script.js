20598e1010ceeb9235aae4d9daaa18d0fd234c25
RiccardoSalvalajo <salvalajo.riccardo.cartiera@gmail.com>
2021-06-07 14:52:29 +0200

    fine palindromo

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// Palidroma: Chiedere all’utente di inserire una parola. Creare una funzione per capire se la parola inserita è palindroma
+
+
+var parola = prompt("inserisci parola");
+
+var parolaInversa = inverti(parola);
+
+
+if(parola == parolaInversa){
+    document.getElementById('pal').innerHTML = 'la parola è palindroma';
+  } else {
+    document.getElementById('pal').innerHTML ='la parola non è palindroma';
+}
+
+
+// Funzioni
+function inverti(x) {
+    var inv ='';
+    for (var i = parola.length - 1; i >= 0; i--) {
+        x = parola[i]
+        inv += x;
+    }
+    return inv;
+}
